1b17370c32f03c458229a544979b44d3a46e5db6
ismailsari07 <xismail.sari@gmail.com>
2025-08-02 10:16:38 -0400

    prayer table

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,157 @@
-import Image from "next/image";
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  GithubIcon,
+  InstagramIcon,
+  MoonIcon,
+  SunIcon,
+  SunriseIcon,
+  SunsetIcon,
+  TwitterIcon,
+  YoutubeIcon,
+} from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="max-w-7xl w-full mx-auto">
+      {/***** Header  *****/}
+      <header
+        id="header"
+        className="flex flex-col items-center gap-6 lg:gap-12 text-center py-16 lg:py-32"
+      >
+        <div className="flex flex-col items-center justify-center gap-6 md:gap-8 text-white">
+          <h1 className="text-5xl md:text-8xl font-header md:px-24">
+            Muhabbetin ve <br /> Mananın Buluştuğu Yer
+          </h1>
+          <p className="px-0 text-center">
+            Toplumumuza manevi gelişim, kültürel bağ ve hayatın her evresinde <br /> destek sunarak
+            hizmet veriyoruz.
+          </p>
+          <div className="flex gap-4">
+            <Button>Namaz Vakitleri</Button>
+            <Button>Support Us ❤️</Button>
+          </div>
+          <div>
+            <Button size="icon" variant="ghost" className="opacity-70">
+              <InstagramIcon />
+            </Button>
+            <Button size="icon" variant="ghost" className="opacity-70">
+              <GithubIcon />
+            </Button>
+            <Button size="icon" variant="ghost" className="opacity-70">
+              <TwitterIcon />
+            </Button>
+            <Button size="icon" variant="ghost" className="opacity-70">
+              <YoutubeIcon />
+            </Button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <video className="w-fit rounded-xl" autoPlay muted loop>
+          <source src="/media/ayasofya.webm" type="video/webm" />
+          Your browser does not support the video
+        </video>
+      </header>
+
+      <section id="prayer-times" className="flex flex-col gap-4 py-16 lg:py-32 text-white">
+        <h2 className="text-4xl md:text-6xl font-header text-left">Namaz Vakitleri</h2>
+        <div className="w-[50%]">
+          Namaz vakitleri, Diyanet İşleri Başkanlığı verilerine dayanarak sunulmaktadır.
+          İbadetlerinizi en doğru zamanda yerine getirmeniz için güvenilir ve resmi kaynaklardan
+          faydalanıyoruz.
+        </div>
+        <div className="flex gap-8 justify-center items-center">
+          <div className="w-[50%] text-xl flex flex-col gap-4 justify-center items-center border border-foreground rounded-lg p-4 text-center">
+            <div>
+              26 Temmuz Cumartesi <br />1 Safer 1447
+            </div>
+
+            <hr className="h-0.5 w-full bg-foreground" />
+
+            <div className="w-full flex gap-4 justify-center items-center">
+              <div className="w-1/2 text-center">
+                <div>Aksam Ezanina Kalan Sure</div>
+                <div>03:20:00</div>
+              </div>
+              <hr className="h-12 w-0.5 bg-foreground" />
+              <div className="w-1/2 text-center">
+                <p>Bilgisayar Saati</p>
+                <p>17:34</p>
+              </div>
+            </div>
+
+            <hr className="h-0.5 w-full bg-foreground" />
+
+            <div className="text-3xl font-semibold text-center underline text-[#FFA500]">
+              Cuma Saati: 2:00
+            </div>
+          </div>
+
+          {/* Namaz Vakitleri */}
+          <div className="w-[50%] flex flex-col gap-3">
+            <div className="flex justify-around items-center text-lg px-4">
+              <div>Namaz</div>
+              <div>Ezan</div>
+              <div>Ikame</div>
+            </div>
+
+            {/* Sabah Namazi */}
+            <div className="flex justify-between items-center border border-foreground rounded-lg py-4 px-16 text-2xl shadow-2xl">
+              <div className="w-28 flex items-center gap-4">
+                <SunriseIcon />
+                <span>Sabah</span>
+              </div>
+
+              <div className="w-28">05:34 a.m</div>
+              <div className="w-28">6:00 a.m</div>
+            </div>
+
+            {/* Ogle Namazi */}
+            <div className="flex justify-between items-center border border-foreground rounded-lg py-4 px-16 text-2xl shadow-2xl animated-gradient">
+              <div className="w-28 flex items-center gap-4">
+                <SunIcon />
+                <span>Ogle</span>
+              </div>
+
+              <div className="w-28">01:15 p.m</div>
+              <div className="w-28">2:00 p.m</div>
+            </div>
+
+            {/* Ikindi Namazi */}
+            <div className="flex justify-between items-center border border-foreground rounded-lg py-4 px-16 text-2xl shadow-2xl">
+              <div className="w-28 flex items-center gap-4">
+                <SunIcon />
+                <span>Ikindi</span>
+              </div>
+
+              <div className="w-28">05:34 p.m</div>
+              <div className="w-28">05:45 p.m</div>
+            </div>
+
+            {/* Aksam Namazi */}
+            <div className="flex justify-between items-center border border-foreground rounded-lg py-4 px-16 text-2xl shadow-2xl">
+              <div className="w-28 flex items-center gap-4">
+                <SunsetIcon />
+                <span>Aksam</span>
+              </div>
+
+              <div className="w-28">08:34 p.m</div>
+              <div className="w-28">08:34 p.m</div>
+            </div>
+
+            {/* Yatsi Namazi */}
+            <div className="flex justify-between items-center border border-foreground rounded-lg py-4 px-16 text-2xl shadow-2xl">
+              <div className="w-28 flex items-center gap-4">
+                <MoonIcon />
+                <span>Yatsi</span>
+              </div>
+
+              <div className="w-28">10:15 p.m</div>
+              <div className="w-28">10:15 p.m</div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { ArrowRight } from 'lucide-react';
+import { Button } from './ui/button';
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-evenly items-center p-6 text-lg">
+      <a href="#">Logo</a>
+
+      <div className="flex items-center gap-4">
+        <a href="#">Dernegimiz</a>
+        <a href="">Hizmetlerimiz</a>
+        <a href="">Bagis</a>
+        <a href="">Faydali LInkler</a>
+      </div>
+
+      <Button>
+      Support Us  ❤️
+      </Button>
+    </nav>
+  );
+}
